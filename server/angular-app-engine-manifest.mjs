1618b645a0b8a501222bb95659f32efb29557c1d
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2025-04-09 16:23:01 +0530

    Auto-generated commit

--- server/angular-app-engine-manifest.mjs
+++ server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/angular-portfolio',
+  basePath: '/Portfolio-Updated',
   supportedLocales: {
   "en-US": ""
 },
--- server/assets-chunks/index_server_html.mjs
+++ server/assets-chunks/index_server_html.mjs
@@ -3,7 +3,7 @@ export default `<!doctype html>
 <head>
   <meta charset="utf-8">
   <title>Yash Mishra Portfolio</title>
-  <base href="/angular-portfolio/">
+  <base href="/Portfolio-Updated/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
